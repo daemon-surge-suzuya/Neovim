09419bc6c2b0b102506bb85b89c44f6838f06929
daemon-surge-suzuya <daemonsurges@gmail.com>
2023-11-15 09:47:31 +0300

    Added nightfox-nvim

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -4,4 +4,5 @@ return {
   { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
   { import = "astrocommunity.git/git-blame-nvim" },
   { import = "astrocommunity.utility/noice-nvim" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim"},
 }
