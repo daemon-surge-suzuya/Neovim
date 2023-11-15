4124ff865c9ed2e62ff4835cecd3eb27c492431a
daemon-surge-suzuya <daemonsurges@gmail.com>
2023-11-15 09:52:15 +0300

    Added astrovim utils

--- lua/user/plugins/core.lua
+++ lua/user/plugins/core.lua
@@ -4,6 +4,7 @@ return {
       "goolord/alpha-nvim",
       opts = function(_, opts)
         opts.section.header.val = require("user/dashboard-images").pikachu
+        local button, get_icon = require("astronvim.utils").alpha_button, require("astronvim.utils").get_icon
         }
         return opts
       end,
