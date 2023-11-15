9c4908bdcbff04e22fffe2b680bb5c5b6eb74d9b
daemon-surge-suzuya <daemonsurges@gmail.com>
2023-11-15 09:46:44 +0300

    Added git-blame-nvim

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -2,4 +2,5 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.rose-pine" },  
   { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
+  { import = "astrocommunity.git/git-blame-nvim" },
 }
